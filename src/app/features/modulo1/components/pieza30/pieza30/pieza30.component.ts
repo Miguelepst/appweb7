b03010c6b11df3df3c300ba55338f826cdffb34d
Miguel Gutierrez <miguelepst@hotmail.com>
2022-11-25 04:33:07 -0500

    commit

---
+++ src/app/features/modulo1/components/pieza30/pieza30/pieza30.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'agit statuopp-pieza30',
+  templateUrl: './pieza30.component.html',
+  styleUrls: ['./pieza30.component.css']
+})
+export class Pieza30Component {
+
+}
